3ae30471d8707f68fcc94371ef2d30c431c50fe7
igor_yazovcev <uSAg9wbxUX6NEROI>
2020-07-06 19:08:20 +0500

    First commit

---
+++ flutter_addition_module/lib/settings_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'bases/base_state.dart';
+
+class SettingsScreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SettingsPage(title: 'Экран настроек'),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  SettingsPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends BaseState<SettingsPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Здесь могли бы быть настройки, но не срослось...',
+            ),
+          ],
+        ),
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
